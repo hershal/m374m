b679895cea3053c6116cf73a05af678068c7c2b1
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-02 14:22:58 -0600

    hw2: added initial files

---
+++ homeworks/hw2/src/program2.m
@@ -0,0 +1,67 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Program 2.
+%
+% A Matlab program that uses the built-in ODE solver 
+% 'ode45' (4th/5th order Runge-Kutta method) to solve 
+% the reaction tank model.
+%
+% Here's how to get started:
+%
+%   1) Copy program2.m (this file) and rtank.m 
+%      into your working directory.
+%
+%   2) Invoke Matlab by typing "matlab" at the 
+%      Linux prompt.
+%
+%   3) Type "program2" in the Matlab window to 
+%      run the program.
+%
+%   4) To complete the homework, use a plain text 
+%      editor to modify the program as necessary.
+%      Once modified, simply re-save the file and
+%      re-run as in Step 3.
+%
+% Note1: The output figures will be saved to your
+%        working directory.
+%
+% Note2: The reaction model must be written in the
+%        general form
+%                 du/dt = F(t,u)
+%        where u is the concentration variable.  
+%        This system is defined in the file rtank.m
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% Define global constants
+global cin eta V gamma ; 
+
+
+%%% Set value of constants for Scenario One %%%
+cin=10^(-2) ; eta=10^(-2) ; V = 10 ; gamma = 10^(-1) ;
+
+% Define initial condition u0
+u0 = 0 ; %this is c0
+
+% Define initial and final times for simulation
+t0 = 0 ;  tf = 10*V/eta  ; 
+
+% Solve ODE system (1) and store results in the 
+% arrays tsolnOne1 and usolnOne1
+[tsolnOne1,usolnOne1]=ode45(@rtank,[t0 tf],u0) ; 
+
+% Solve ODE system (2) [set gamma=0] and store 
+% results in the arrays tsolnOne2 and usolnOne2
+gamma = 0 ;
+[tsolnOne2,usolnOne2]=ode45(@rtank,[t0 tf],u0) ; 
+
+% Plot u-values versus t-values for each system
+figure(1) ; %open figure
+clf ; % clear figure 
+hold on ; % hold figure for multiple plots
+plot(tsolnOne1,usolnOne1,'b-')  ; %plot u versus t
+plot(tsolnOne2,usolnOne2,'r--') ; %plot u versus t
+xlabel('t')      ; %label x axis
+ylabel('c')      ; %label y axis
+title('Scenario One: Ode 1 (blue,solid), Ode 2 (red,dash)') ;
+print -f1 -dpdf prog2_fig1.pdf ; %print figure to a file
+
---
+++ homeworks/hw2/src/rtank.m
@@ -0,0 +1,27 @@
+
+function dudt = F(t,u)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Evaluates du/dt for the reaction tank model
+%
+%      du/dt = (eta/V)*(cin - u) - gamma*u^2.
+%
+% Inputs:  t = time
+%          u = concentration.
+%
+% Output:  dudt = derivative of concentration.
+%
+% Note: Matlab's ODE solvers require us to include the
+% variable t in the argument list of our function F even
+% though it might not be used to evaluate dudt.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Access global constants from main prog
+global cin eta V gamma ;        
+
+% Initialize output variable dudt
+dudt = 0 ; 
+
+% Evaluate dudt for given t and u
+dudt = (eta/V)*(cin - u) - gamma*u^2 ;
+ 
