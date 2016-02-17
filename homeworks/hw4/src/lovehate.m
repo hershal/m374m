05eb74a0f6e6d3e472863a9aa134f8a5560ff89c
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-17 00:09:31 -0600

    hw4: added initial files from hw drop

---
+++ homeworks/hw4/src/lovehate.m
@@ -0,0 +1,31 @@
+
+function dudt = F(t,u)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Evaluates du/dt for the relationship model
+%
+%      du1/dt = a u1 + b u2 
+%      du2/dt = c u1 + d u2.
+%
+% Inputs:  t = time
+%          u = system vector (2x1 array) where
+%              u(1)=u1=x, u(2)=u2=y.
+%
+% Output:  dudt = derivative of system vector (2x1 array), 
+%                 where dudt(1)=du1/dt, dudt(2)=du2/dt.
+%
+% Note: Matlab's ODE solvers require us to include the
+% variable t in the argument list of our function F even
+% though it might not be used to evaluate dudt.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Access global constants from main prog
+global a b c d ;        
+
+% Initialize output array dudt
+dudt = zeros(2,1) ; 
+
+% Evaluate dudt for given t and u
+dudt(1) = a*u(1) + b*u(2) ;
+dudt(2) = c*u(1) + d*u(2) ;
+ 
---
+++ homeworks/hw4/src/program4.m
@@ -0,0 +1,86 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Program 4.
+%
+% A Matlab program that uses the built-in ODE solver 
+% 'ode45' (4th/5th order Runge-Kutta method) to solve 
+% the relationship model.
+%
+% Here's how to get started:
+%
+%   1) Copy program4.m (this file) and lovehate.m 
+%      into your working directory.
+%
+%   2) Invoke Matlab by typing "matlab" at the 
+%      Linux prompt.
+%
+%   3) Type "program4" in the Matlab window to 
+%      run the program.
+%
+%   4) To complete the homework, use a plain text 
+%      editor to modify the program as necessary.
+%      Once modified, simply re-save the file and
+%      re-run as in Step 3.
+%
+% Note1: The output figures will be saved to your
+%        working directory.
+%
+% Note2: The relationship model must be written in 
+%        the general first-order form
+%                 du1/dt = F1(t,u1,u2)
+%                 du2/dt = F2(t,u1,u2)
+%        where u1=x, u2=y.  In vector notation, 
+%        this is du/dt = F(t,u), where u=(u1,u2), 
+%        F=(F1,F2).  This system is defined in the 
+%        file lovehate.m
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+% Define global constants
+global a b c d ; 
+
+% Define initial condition u0 (fill in values later)
+u0 = zeros(2,1); %2x1 array
+
+% Define initial and final times 
+t0 = 0 ;  tf = 8  ; 
+
+% Set value of constants
+a = 0 ; b = 1 ; c = -2 ; d = 0 ;
+
+% Define number and values of (x0,y0) points
+Npoints = 4 ;
+x0 = [1.0, -1.1, -2.3, 1.5] ;
+y0 = [0.8,  1.7, -0.9, -1.1] ;
+
+% Define and clear figures
+figure(1) ; clf ; hold on ;
+figure(2) ; clf ; hold on ;
+figure(3) ; clf ; hold on ;
+
+% Solve system for each initial condition and
+% plot results
+
+for i=1:Npoints
+  u0(1) = x0(i) ; u0(2) = y0(i) ;
+  [tsoln,usoln]=ode45(@lovehate,[t0 tf],u0) ; 
+  figure(1) ; 
+  plot(tsoln,usoln(:,1),'b--') ; %plot u1 versus t
+  figure(2) ; 
+  plot(tsoln,usoln(:,2),'k-.') ; %plot u2 versus t
+  figure(3) ;
+  plot(usoln(:,1),usoln(:,2),'r-')  ; %plot u2 versus u1 
+  clear tsoln usoln ;
+end
+
+figure(1) ; xlabel('t') ; ylabel('x') ;
+figure(1) ; title('Time view: x vs t for various (x0,y0)') ;
+print -f1 -dpdf prog4_fig1.pdf ; %print figure to a file
+
+figure(2) ; xlabel('t') ; ylabel('y') ;
+figure(2) ; title('Time view: y vs t for various (x0,y0)') ;
+print -f2 -dpdf prog4_fig2.pdf ; %print figure to a file
+
+figure(3) ; xlabel('x') ; ylabel('y') ;
+figure(3) ; title('Phase view: y vs x for various (x0,y0)') ;
+print -f3 -dpdf prog4_fig3.pdf ; %print figure to a file
+
