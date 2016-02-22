23c3656debd62064a579c52f5b84a644cbfd97b0
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-21 21:38:44 -0600

    hw4: added src cmakelists framework

--- homeworks/hw4/src/generate_2_1_5.m
+++ homeworks/hw4/src/generate_2_1_5.m
@@ -8,11 +8,11 @@ vopt = odeset ("InitialStep", 1e-1, "MaxStep", 1e-1, "RelTol", 1e-3, "AbsTol", 1
 x0 = [-0.5, 0, 0.5, 2, 3, 5, 7];
 y0 = [-0.5, 0, 0.5, 2, 3, 5, 7];
 
-figure(1); clf; hold on;
+## figure(1); clf; hold on;
 for i=1:length(x0)
   for j=1:length(y0)
     [t, u] = ode45(f_2_1_5, [0,3], [x0(i), y0(j)], vopt);
-    plot(u(:,1),u(:,2));
+    ## plot(u(:,1),u(:,2));
     u = [u zeros(size(u,1),1)];
     dlmwrite(sprintf("2.1.5-%i-%i.mat", i, j), u, " ");
     clear t, u;
