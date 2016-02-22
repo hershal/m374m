16c46d671670c91a342c88dc888db5fe3e42e019
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-22 02:43:07 -0600

    hw4: cleaned up lovehate

--- homeworks/hw4/src/lovehate.m
+++ homeworks/hw4/src/lovehate.m
@@ -1,17 +1,17 @@
 
-function dudt = F(t,u)
+function dudt = lovehate(t,u)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Evaluates du/dt for the relationship model
 %
-%      du1/dt = a u1 + b u2 
+%      du1/dt = a u1 + b u2
 %      du2/dt = c u1 + d u2.
 %
 % Inputs:  t = time
 %          u = system vector (2x1 array) where
 %              u(1)=u1=x, u(2)=u2=y.
 %
-% Output:  dudt = derivative of system vector (2x1 array), 
+% Output:  dudt = derivative of system vector (2x1 array),
 %                 where dudt(1)=du1/dt, dudt(2)=du2/dt.
 %
 % Note: Matlab's ODE solvers require us to include the
@@ -20,10 +20,10 @@ function dudt = F(t,u)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Access global constants from main prog
-global a b c d ;        
+global a b c d ;
 
 % Initialize output array dudt
-dudt = zeros(2,1) ; 
+dudt = zeros(2,1) ;
 
 % Evaluate dudt for given t and u
 dudt(1) = a*u(1) + b*u(2) ;
