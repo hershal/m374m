2fd2aa28227ffd9ae353aab4edf031a1f0c2f370
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-21 23:10:05 -0600

    hw4: elaborate on equilibria on 2.1.5

--- homeworks/hw4/src/generate_2_1_5.m
+++ homeworks/hw4/src/generate_2_1_5.m
@@ -5,13 +5,13 @@ pkg load odepkg;
 ## \addplot3[gray, quiver={u={}, v={}, scale arrows=0.1}, -stealth,samples=15]{0};
 f_2_1_5 = @(t, u) [u(2)-u(1), -u(2)+(5*u(1)^2)/(4+u(1)^2)];
 vopt = odeset ("InitialStep", 1e-1, "MaxStep", 1e-1, "RelTol", 1e-3, "AbsTol", 1e-3);
-x0 = [-0.5, 0, 0.5, 2, 3, 5, 7];
-y0 = [-0.5, 0, 0.5, 2, 3, 5, 7];
+x0 = [-2, 0, 2, 3, 5, 8];
+y0 = [-2, 0, 2, 3, 5, 8];
 
 ## figure(1); clf; hold on;
 for i=1:length(x0)
   for j=1:length(y0)
-    [t, u] = ode45(f_2_1_5, [0,3], [x0(i), y0(j)], vopt);
+    [t, u] = ode45(f_2_1_5, [0,8], [x0(i), y0(j)], vopt);
     ## plot(u(:,1),u(:,2));
     u = [u zeros(size(u,1),1)];
     dlmwrite(sprintf("../resources/2.1.5-%i-%i.mat", i, j), u, " ");
