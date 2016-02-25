26f6c567cdf06af34e318c3cbe799e5ca64b2e28
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-25 01:22:01 -0600

    hw4: 2.2.1a custom generator points

--- homeworks/hw4/src/generate_2_2_1a.m
+++ homeworks/hw4/src/generate_2_2_1a.m
@@ -3,4 +3,6 @@
 
 f_2_2_1a = @(t, u) [u(1)-3*u(2), -3*u(1)+u(2)];
 problem = "2.2.1a";
-generate(f_2_2_1a, problem);
+points = [-2, -0.67, -0.33, 0.33,  0.67, 2;
+          -2, -0.67, -0.33, 0.33,  0.67, 2;];
+generate(f_2_2_1a, problem, points);
