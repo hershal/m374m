3b82d1f03b17dab381b5ab408f7f46ec2e53c45b
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-25 01:23:03 -0600

    hw4: 2.1.5 custom generator points

--- homeworks/hw4/src/generate_2_1_5.m
+++ homeworks/hw4/src/generate_2_1_5.m
@@ -3,4 +3,6 @@
 
 f_2_1_5 = @(t, u) [u(2)-u(1), -u(2)+(5*u(1)^2)/(4+u(1)^2)];
 problem = "2.1.5";
-generate(f_2_1_5, problem)
+points = [-2, 0, 2, 3, 5, 8;
+          -2, 0, 2, 3, 5, 8];
+generate(f_2_1_5, problem, points);
