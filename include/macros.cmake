431f26599298eb5030adb90f1070697d5040ddc9
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-24 18:01:32 -0600

    include, hw4: added generator function

--- include/macros.cmake
+++ include/macros.cmake
@@ -3,3 +3,11 @@ add_custom_command(OUTPUT macros.sty
   DEPENDS ${PROJECT_SOURCE_DIR}/include/macros.sty
   COMMAND ${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/include/macros.sty ./macros.sty
 )
+
+function (add_generator generator)
+  add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${generator}.timestamp
+    DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${generator}.m ${CMAKE_CURRENT_SOURCE_DIR}/generate.m
+    COMMAND "octave" "-p" "${CMAKE_CURRENT_SOURCE_DIR}" ${CMAKE_CURRENT_SOURCE_DIR}/${generator}.m
+    COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_CURRENT_BINARY_DIR}/${generator}.timestamp
+    )
+endfunction()
