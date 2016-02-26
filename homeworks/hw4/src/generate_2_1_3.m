7819479b455650e0842f28beb2ae3954e60f6f21
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-26 15:08:11 -0600

    hw4: 2.1.3 generator works now

--- homeworks/hw4/src/generate_2_1_3.m
+++ homeworks/hw4/src/generate_2_1_3.m
@@ -3,5 +3,7 @@
 
 f_2_1_3 = @(t, u) [u(2)^2, -2/3*u(1)];
 problem = "2.1.3";
-## does not work
-generate_inorder(f_2_1_3, problem);
+points = [-1, -0.5, 0.001, -1, -1;
+          -1, -1, -0.001, -0.5, -0.75];
+tf = [50, 2, 40, 6, 8];
+generate_inorder(f_2_1_3, problem, points, tf);
