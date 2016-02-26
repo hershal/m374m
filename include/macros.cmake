57f82dca4aff873abd94b087768dbe965935c00f
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-25 20:13:33 -0600

    project: abstracted homework macros, updated hw1,4

--- include/macros.cmake
+++ include/macros.cmake
@@ -1,7 +1,15 @@
 include(${PROJECT_SOURCE_DIR}/include/UseLATEX.cmake)
-add_custom_command(OUTPUT macros.sty
-  DEPENDS ${PROJECT_SOURCE_DIR}/include/macros.sty
+
+add_custom_command(OUTPUT homework-macros.sty
+  DEPENDS ${PROJECT_SOURCE_DIR}/include/macros.sty ${PROJECT_SOURCE_DIR}/include/homework-macros.sty
   COMMAND ${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/include/macros.sty ./macros.sty
+  COMMAND ${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/include/homework-macros.sty ./homework-macros.sty
+)
+
+add_custom_command(OUTPUT cheat-sheet-macros.sty
+  DEPENDS ${PROJECT_SOURCE_DIR}/include/macros.sty ${PROJECT_SOURCE_DIR}/include/cheat-sheet-macros.sty
+  COMMAND ${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/include/macros.sty ./macros.sty
+  COMMAND ${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/include/cheat-sheet-macros.sty ./cheat-sheet-macros.sty
 )
 
 function (add_generator generator)
