206bed6d53fdc3231c9aa7138892a61cdddb70c3
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-26 15:07:42 -0600

    hw4/generate_inorder: cleanups

--- homeworks/hw4/src/generate_inorder.m
+++ homeworks/hw4/src/generate_inorder.m
@@ -11,11 +11,11 @@ function generate_inorder(f, problem, points = [-1, -0.5, 0; -1, -1, 0], tf = [4
 
   assert(length(x0) == length(y0));
 
-  ## figure(1); clf; hold on;
+  ## figure(1); clf; hold on; axis([-1 1 -1 1])
   for i=1:length(x0)
     [t, u] = ode45(f, [t0, tf(i)], [x0(i), y0(i)], vopt);
     u = [u zeros(size(u,1),1)];
-    ## plot(u(:, 1), u(:,1))
+    ## plot(u(:, 1), u(:,2))
     dlmwrite(sprintf("%s-%i.mat", problem, i), u, " ", "precision", "%.5f");
     clear t, u;
   endfor
