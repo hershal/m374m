f2f4f36d4a57c7a76a151fd949aa0a1c25f56766
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-29 13:35:09 -0600

    hw5/src/generate_2_4_1e: added both cases

--- homeworks/hw5/src/generate_2_4_1e.m
+++ homeworks/hw5/src/generate_2_4_1e.m
@@ -2,8 +2,14 @@
 # -*- mode: octave -*-
 
 f_2_4_1e = @(t, u) [u(1)^2+u(2)^2-4, u(2)-2*u(1)];
-problem = "2.4.1e";
-points = [-2, 0, 2;
-          -2, 0, 2];
-generate_inorder(f_2_4_1e, problem, points, [2 1 0.5]);
-## generate(f_2_4_1e, problem);
+prob0 = "2.4.1e";
+prob1 = "2.4.1e";
+ep = 0.01;
+z0 = [2/sqrt(5), 4/sqrt(5)];
+z1 = [-2/sqrt(5), -4/sqrt(5)];
+p0 = [z0(1)+ep, z0(1)+ep, z0(1)-ep, z0(1)-ep
+      z0(2)+ep, z0(2)-ep, z0(2)-ep, z0(2)+ep];
+p1 = [z1(1)+ep, z1(1)+ep, z1(1)-ep, z1(1)-ep
+      z1(2)+ep, z1(2)-ep, z1(2)-ep, z1(2)+ep];
+generate_inorder(f_2_4_1e, prob0, p0, [3 3 3 3]);
+generate_inorder(f_2_4_1e, prob1, p1, [1 0.75 1 0.75]);
