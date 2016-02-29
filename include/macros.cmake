ee942b0e6df798465fd5bdbf0d308b2139f94e0a
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-29 12:57:12 -0600

    project: generalized generate and generate_inorder

--- include/macros.cmake
+++ include/macros.cmake
@@ -15,7 +15,7 @@ add_custom_command(OUTPUT cheat-sheet-macros.sty
 function (add_generator generator)
   add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${generator}.timestamp
     DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${generator}.m ${ARGN}
-    COMMAND "octave" "-p" "${CMAKE_CURRENT_SOURCE_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}/${generator}.m"
+    COMMAND "octave" "-p" "${CMAKE_CURRENT_SOURCE_DIR}" "-p" "${common_dir}" "${CMAKE_CURRENT_SOURCE_DIR}/${generator}.m"
     COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_CURRENT_BINARY_DIR}/${generator}.timestamp
     )
 endfunction()
