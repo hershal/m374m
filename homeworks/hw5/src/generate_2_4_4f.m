2fb7bcbc1a44d9912022999f969651046782c020
Hershal Bhave <hershal.bhave@gmail.com>
2016-03-01 12:35:51 -0600

    hw5: added WIP generate_2_4_4f for office hours

---
+++ homeworks/hw5/src/generate_2_4_4f.m
@@ -0,0 +1,9 @@
+#!/usr/bin/env octave
+# -*- mode: octave -*-
+
+mu = -1;
+f_2_4_4f = @(t,u) [u(2), u(1)^2-u(1)+mu*u(2)];
+prob0 = "2.4.4f-1"
+[x, y] = meshgrid (-3:0.1:3);
+figure(1); clf; hold on; axis([-3 3 -3 3]);
+quiver(x, y, y, x^2-x+mu*y);
