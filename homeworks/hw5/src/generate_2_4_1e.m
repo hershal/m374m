4d9ac26f5b0ca09a7b70dce9c77009838c8ff2c9
Hershal Bhave <hershal.bhave@gmail.com>
2016-02-29 21:33:34 -0600

    hw5/generate_2_4_1e: fine tuning plots

--- homeworks/hw5/src/generate_2_4_1e.m
+++ homeworks/hw5/src/generate_2_4_1e.m
@@ -5,11 +5,12 @@ f_2_4_1e = @(t, u) [u(1)^2+u(2)^2-4, u(2)-2*u(1)];
 prob0 = "2.4.1e-1";
 prob1 = "2.4.1e-2";
 ep = 0.01;
+sp = 0.2;
 z0 = [2/sqrt(5), 4/sqrt(5)];
 z1 = [-2/sqrt(5), -4/sqrt(5)];
 p0 = [z0(1)+ep, z0(1)+ep, z0(1)-ep, z0(1)-ep
       z0(2)+ep, z0(2)-ep, z0(2)-ep, z0(2)+ep];
-p1 = [z1(1)+ep, z1(1)+ep, z1(1)-ep, z1(1)-ep
-      z1(2)+ep, z1(2)-ep, z1(2)-ep, z1(2)+ep];
+p1 = [z1(1)+sp, z1(1)+sp, z1(1)-sp, z1(1)-sp
+      z1(2)+sp, z1(2)-sp, z1(2)-sp, z1(2)+sp];
 generate_inorder(f_2_4_1e, prob0, p0, [3 3 3 3]);
-generate_inorder(f_2_4_1e, prob1, p1, [1 0.75 1 0.75]);
+generate_inorder(f_2_4_1e, prob1, p1, [1.1 .7 1.05 .7]);
