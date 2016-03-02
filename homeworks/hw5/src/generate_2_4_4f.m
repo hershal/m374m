d03745e1b4f4ae6bfad0ee525cb33dccec5e0bdc
Hershal Bhave <hershal.bhave@gmail.com>
2016-03-01 18:28:13 -0600

    hw5: fixed quiver bug in generate_2_4_4f

--- homeworks/hw5/src/generate_2_4_4f.m
+++ homeworks/hw5/src/generate_2_4_4f.m
@@ -6,4 +6,4 @@ f_2_4_4f = @(t,u) [u(2), u(1)^2-u(1)+mu*u(2)];
 prob0 = "2.4.4f-1"
 [x, y] = meshgrid (-3:0.1:3);
 figure(1); clf; hold on; axis([-3 3 -3 3]);
-quiver(x, y, y, x^2-x+mu*y);
+quiver(x, y, y, x.^2-x+mu.*y);
