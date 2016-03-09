5cfada1c989e00bb04efd447caf2831fb750badb
Hershal Bhave <hershal.bhave@gmail.com>
2016-03-09 09:31:33 -0600

    hw6: tweaked plot points for 2.6.3.5

--- homeworks/hw6/src/generate_2_6_3_5.m
+++ homeworks/hw6/src/generate_2_6_3_5.m
@@ -7,12 +7,12 @@ N = 20;
 mu = b/2;
 f_2_6_3_5 = @(t, u) [-a*u(1)*u(2)+mu*(N-u(1)-u(2)), a*u(1)*u(2)-b*u(2)];
 problem = "2.6.3.5";
-points = [1.5 1.5 .5 .75 -1  -2
-           -1  2   -1 -1  .75  1.75];
-tols = [3 4 3 3 4 5];
+points = [5 30 30   5  5  30
+          4 2  0.75 1  -1 -0.5];
+tols = [5 6 7 8 2.8 1];
 rets = generate_inorder(f_2_6_3_5, problem, points, tols);
 
-figure(1); clf; hold on;
+figure(1); clf; hold on; axis([5 30 -2 4]);
 for i=1:size(rets)
   plot(rets{i}(:,1), rets{i}(:,2))
 endfor
