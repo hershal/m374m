6bfa0a4c228ad812431d1554a228734bd65887db
Hershal Bhave <hershal.bhave@gmail.com>
2016-03-21 17:36:50 -0500

    common: generate{,_inorder} set relative stepsizes

--- common/generate.m
+++ common/generate.m
@@ -4,7 +4,6 @@
 function ret = generate(f, problem, points = [-2, 0, 2; -2, 0, 2], tf=6)
   pkg load odepkg;
 
-  vopt = odeset ("InitialStep", 1e-2, "MaxStep", 1e-2, "RelTol", 1e-3, "AbsTol", 1e-3);
   x0 = points(1, :);
   y0 = points(2, :);
   t0 = 0;
@@ -12,6 +11,7 @@ function ret = generate(f, problem, points = [-2, 0, 2; -2, 0, 2], tf=6)
   ret = cell(length(x0),length(y0));
   for i=1:length(x0)
     for j=1:length(y0)
+      vopt = odeset ("InitialStep", (tf-t0)/1e2, "MaxStep", (tf-t0)/1e2, "RelTol", 1e-3, "AbsTol", 1e-3);
       [t, u] = ode45(f, [t0, tf], [x0(i), y0(j)], vopt);
       u = [u zeros(size(u,1),1)];
       ret{i,j} = [u(:,1), u(:,2)];
--- common/generate_inorder.m
+++ common/generate_inorder.m
@@ -4,7 +4,6 @@
 function ret = generate_inorder(f, problem, points = [], tf = [])
   pkg load odepkg;
 
-  vopt = odeset ("InitialStep", 1e-2, "MaxStep", 1e-2, "RelTol", 1e-3, "AbsTol", 1e-3);
   x0 = points(1, :);
   y0 = points(2, :);
   t0 = 0;
@@ -13,6 +12,7 @@ function ret = generate_inorder(f, problem, points = [], tf = [])
 
   ret = cell(length(x0),1);
   for i=1:length(x0)
+    vopt = odeset ("InitialStep", (tf(i)-t0)/1e2, "MaxStep", (tf(i)-t0)/1e2, "RelTol", 1e-3, "AbsTol", 1e-3);
     [t, u] = ode45(f, [t0, tf(i)], [x0(i), y0(i)], vopt);
     u = [u zeros(size(u,1),1)];
     ret{i} = [u(:,1), u(:,2)];
