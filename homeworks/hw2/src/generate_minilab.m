70a9070090da2678d43bde1e1d2ca6d3f9755150
Hershal Bhave <hershal.bhave@gmail.com>
2016-03-21 15:16:00 -0500

    hw2/generate_minilab: cleaned up; works now

--- homeworks/hw2/src/generate_minilab.m
+++ homeworks/hw2/src/generate_minilab.m
@@ -12,24 +12,24 @@ function dumpMinilab(prob)
 
   % Solve ODE system (1)
   [tsoln1,usoln1] = ode45(@rtank,[t0 tf], u0, vopt);
-  t = [tsoln1 usoln1(:,1) zeros(size(usoln1,1),1)];
-  dlmwrite(sprintf("minilab-%i-t.mat", prob), usoln1, " ", "precision", "%.5f");
+  s1 = [tsoln1 usoln1(:,1) zeros(size(usoln1,1),1)];
+  dlmwrite(sprintf("minilab-%i-1.mat", prob), s1, " ", "precision", "%.5f");
 
   % Solve ODE system (2) [set gamma=0]
   gamma = 0;
   [tsoln2,usoln2] = ode45(@rtank, [t0 tf], u0, vopt);
-  t_approx = [tsoln2 usoln2(:,1) zeros(size(usoln2,1),1)];
-  dlmwrite(sprintf("minilab-%i-t_approx.mat", prob), usoln2, " ", "precision", "%.5f");
+  s2 = [tsoln2 usoln2(:,1) zeros(size(usoln2,1),1)];
+  dlmwrite(sprintf("minilab-%i-2.mat", prob), s2, " ", "precision", "%.5f");
 
-  figure(1); clf; hold on;
-  plot(tsoln1,usoln1,'b-')  ; %plot u versus t
-  plot(tsoln2,usoln2,'r--') ; %plot u versus t
+  ## figure(1); clf; hold on;
+  ## plot(tsoln1,usoln1,'b-')  ; %plot u versus t
+  ## plot(tsoln2,usoln2,'r--') ; %plot u versus t
 endfunction
 
 global cin eta V gamma
 c1 = [10^(-2), 10^(-2), 10, 10^(-1)];
 c2 = [10^(-2), 10^(-1), 1, 10^(-1)];
-configs = [c1 c2];
+configs = [c1; c2];
 
 for i=1:size(configs,1)
   cc = configs(i,:);
