02d1cf4c435a29440e1acdcc39f16568e896bd67
Hershal Bhave <hershal.bhave@gmail.com>
2016-03-31 13:51:49 -0500

    hw8: converted src to octave; line cleanup

--- homeworks/hw8/src/mercury.m
+++ homeworks/hw8/src/mercury.m
@@ -1,5 +1,5 @@
-
-function dudt = F(t,u)
+% -*- mode: octave -*-
+function dudt = mercury(t,u)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Evaluates du/dt for the Mercury model
@@ -11,7 +11,7 @@ function dudt = F(t,u)
 %          u = system vector (2x1 array) where
 %              u(1)=(umerc), u(2)=d(umerc)/dtheta.
 %
-% Output:  dudt = derivative of system vector (2x1 array), 
+% Output:  dudt = derivative of system vector (2x1 array),
 %                 where dudt(1)=du1/dt, etc.
 %
 % Note: Matlab's ODE solvers require us to include the
@@ -20,12 +20,11 @@ function dudt = F(t,u)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Access global constants from main prog
-global rho gamma c ;        
+global rho gamma c ;
 
 % Initialize output array dudt
-dudt = zeros(2,1) ; 
+dudt = zeros(2,1) ;
 
 % Evaluate dudt for given t and u
 dudt(1) = u(2) ;
 dudt(2) = -u(1) + (1/rho) + (gamma*u(1)*u(1)/rho) ;
- 
--- homeworks/hw8/src/program8.m
+++ homeworks/hw8/src/program8.m
@@ -1,22 +1,23 @@
+% -*- mode: octave -*-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Program 8.
 %
-% A Matlab program that uses the built-in ODE solver 
-% 'ode45' (4th/5th order Runge-Kutta method) to solve 
+% A Matlab program that uses the built-in ODE solver
+% 'ode45' (4th/5th order Runge-Kutta method) to solve
 % the relativistic model for Mercury's orbit.
 %
 % Here's how to get started:
 %
-%   1) Copy program8.m (this file) and mercury.m 
+%   1) Copy program8.m (this file) and mercury.m
 %      into your working directory.
 %
-%   2) Invoke Matlab by typing "matlab" at the 
+%   2) Invoke Matlab by typing "matlab" at the
 %      Linux prompt.
 %
-%   3) Type "program8" in the Matlab window to 
+%   3) Type "program8" in the Matlab window to
 %      run the program.
 %
-%   4) To complete the homework, use a plain text 
+%   4) To complete the homework, use a plain text
 %      editor to modify the program as necessary.
 %      Once modified, simply re-save the file and
 %      re-run as in Step 3.
@@ -24,27 +25,28 @@
 % Note1: The output figures will be saved to your
 %        working directory.
 %
-% Note2: The Mercury model must be written in 
+% Note2: The Mercury model must be written in
 %        the general first-order form
 %                 du1/dt = F1(t,u1,u2)
 %                 du2/dt = F2(t,u1,u2)
-%        where t=theta, u1=(umerc), u2=d(umerc)/dt.  
-%        In vector notation, this is du/dt = F(t,u), 
-%        where u=(u1,u2), F=(F1,F2). This system is 
-%        defined in the file mercury.m.  Here umerc 
+%        where t=theta, u1=(umerc), u2=d(umerc)/dt.
+%        In vector notation, this is du/dt = F(t,u),
+%        where u=(u1,u2), F=(F1,F2). This system is
+%        defined in the file mercury.m.  Here umerc
 %        is the inverse radius variable.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+pkg load odepkg;
 
 % Define global constants
-global rho gamma c ; 
+global rho gamma c ;
 
 % Define initial condition array (fill in values later)
 uinit = zeros(2,1); %2x1 array
 
-% Define initial and final theta 
-n = 10 ;  
-t0 = pi/4 ;  tf = t0 + 2*n*pi ; 
+% Define initial and final theta
+n = 10 ;
+t0 = pi/4 ;  tf = t0 + 2*n*pi ;
 
 % Set value of constants
 rho = 1 ; gamma = 0.0 ;  c = 0.7 ;
@@ -56,10 +58,10 @@ uinit = [umerc0, umercdot0] ;
 % Specify error tolerances for ODE solver
 options = odeset('AbsTol',1e-08,'RelTol',1e-06) ;
 
-% Solve system 
-[tsoln,usoln]=ode45(@mercury,[t0 tf],uinit,options) ; 
+% Solve system
+[tsoln,usoln]=ode45(@mercury,[t0 tf],uinit,options) ;
 
-% Compute (x,y)-values for each theta-value 
+% Compute (x,y)-values for each theta-value
 N = length(tsoln) ;
 xsoln = zeros(1,N) ;
 ysoln = zeros(1,N) ;
@@ -80,4 +82,3 @@ title('Orbit curve: y vs x') ;
 xsun=0 ; ysun=0 ;
 text(xsun,ysun,'SUN','HorizontalAlignment','left') ;
 print -f1 -dpdf prog8_fig1.pdf ; %print figure to a file
-
